a726bb80dabb749984195b3b613c8509ac226e0c
Victor0920 <vvidal@snapfinance.co.uk>
2025-05-24 14:35:25 +0200

    Config updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,30 +1,6 @@
 return {
   'nvim-lua/plenary.nvim',
   'christoomey/vim-tmux-navigator', -- tmux split and window navigation
-  -- 'szw/vim-maximizer',
-  -- 'tpope/vim-surround',
-  -- 'vim-scripts/ReplaceWithRegister',
-  -- 'numToStr/Comment.nvim',
-  -- 'nvim-lualine/lualine.nvim',
-  -- 'hrsh7th/cmp-nvim-lsp',
-  -- 'hrsh7th/nvim-cmp',
-  -- 'hrsh7th/cmp-buffer',
-  -- 'hrsh7th/cmp-path',
-  -- 'L3MON4D3/LuaSnip',
-  -- 'saadparwaiz1/cmp_luasnip',
-  -- 'rafamadriz/friendly-snippets',
-  -- {
-  --   'windwp/nvim-autopairs',
-  --   opts = {
-  --     check_ts = true, -- enable tree sitter
-  --     ts_config = {
-  --       lua = { 'string' }, -- dont add pairs in lua string treesitter nodes
-  --       javascript = { 'template_string' }, -- dont add pairs in javascript template_string
-  --       java = false, -- dont check treesitter on java
-  --     },
-  --   },
-  -- },
-  -- 'windwp/nvim-ts-autotag',
   'hrsh7th/cmp-nvim-lsp',
   {
     'glepnir/lspsaga.nvim',
